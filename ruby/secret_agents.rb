3a6c2674098f4754f4d28f1b0b441658621e2c9a
galessio <alessio.gabriela@gmail.com>
2017-06-01 15:09:12 -0700

    Add pseudocode for encrypt and decrypt method

---
+++ ruby/secret_agents.rb
@@ -0,0 +1,18 @@
+# Encrypt method Pseudo code
+#=============
+# Prompt user input and store value in a variable
+# For every character in the string (for index 0 to length of string)do the following
+# If character is space
+# do nothing
+# Otherwise
+# replace character with character [index + 1]
+
+# Decrypt method Pseudo code
+#=======
+# Prompt user input and store value in a variable
+# For every character in the string (for index 0 to length of string)do the following
+# If character is space
+# do nothing
+# Otherwise
+# replace character with character [index -1]
+
