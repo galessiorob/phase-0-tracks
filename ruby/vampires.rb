9629555173c228ec91ef56c73cd260300d2fb3d6
galessio <alessio.gabriela@gmail.com>
2017-06-04 00:49:37 -0700

    Add boolean detection logic to questions

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -20,12 +20,41 @@
 #     Probably a vampire
 
 #==========================================
-#Variables
+#Test Variables
 wolves_like_sunshine = true
 wolves_like_garlic = true
 vampires_like_sunshine = false
 vampires_like_garlic = false
 
+#Variable tests
+# p wolves_like_garlic && wolves_like_sunshine
+# #true
+# p wolves_like_garlic || vampires_like_garlic
+# #true
+# p wolves_like_garlic && vampires_like_garlic
+# #false
+# p wolves_like_garlic && (vampires_like_sunshine || vampires_like_garlic)
+# #false
+# p (wolves_like_garlic && vampires_like_garlic) || wolves_like_sunshine
+# #true
+# p vampires_like_garlic
+# #false
+# p !vampires_like_garlic
+# #true
+# p !(wolves_like_sunshine && wolves_like_garlic)
+# #false
+
+#=========================================
+#Program Variables
+username = []
+age = []
+year = []
+garlicbread = []
+garlicbreadb =[]
+insurance = []
+insuranceb = []
+
+#==========================================
 #Driving Code
 
 puts "What's your name?"
@@ -37,27 +66,38 @@ age = gets.to_i
 puts "What year were you born?"
 year = gets.to_i
 
+realage = 2017 - age
+agecheck =[]
+
+if realage == year
+  agecheck = TRUE
+else
+  agecheck = FALSE
+end
+#p agecheck
+
 puts "Would you like to have some garlic bread? Yes or No?"
-garlicbread = gets.chomp
+case (garlicbread = gets.chomp)
+when 'Yes'
+  garlicbread = true
+when 'No'
+  garlicbread = false
+else
+  p 'Not sure'
+end
+print garlicbread
 
 puts "Would you like to enroll in the company's insurance? Yes or No?"
-insurance = gets.chomp
+case (insurance = gets.chomp)
+when "Yes"
+  insurance = true
+when 'No'
+  insurance = false
+else
+  p 'Not sure'
+end
+print insurance
 
-#Variable tests
-p wolves_like_garlic && wolves_like_sunshine
-#true
-p wolves_like_garlic || vampires_like_garlic
-#true
-p wolves_like_garlic && vampires_like_garlic
-#false
-p wolves_like_garlic && (vampires_like_sunshine || vampires_like_garlic)
-#false
-p (wolves_like_garlic && vampires_like_garlic) || wolves_like_sunshine
-#true
-p vampires_like_garlic
-#false
-p !vampires_like_garlic
-#true
-p !(wolves_like_sunshine && wolves_like_garlic)
-#false
+
+
 
