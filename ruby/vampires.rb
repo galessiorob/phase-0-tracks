2e7b3103e9d0ef74b6a93c1f606fda1266451c92
galessio <alessio.gabriela@gmail.com>
2017-06-03 19:47:05 -0700

    Add comparisson variables

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -20,6 +20,12 @@
 #     Probably a vampire
 
 #==========================================
+#Variables
+wolves_like_sunshine = true
+wolves_like_garlic = true
+vampires_like_sunshine = false
+vampires_like_garlic = false
+
 #Driving Code
 
 puts "What's your name?"
