5f8abc0c22e3f638fa95bf00b0a0fa33dd19882c
galessio <alessio.gabriela@gmail.com>
2017-06-08 18:41:29 -0700

    Add code for Fibonacci sequence generating method

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -24,7 +24,32 @@ def search_array (arr,number)
   hash.key(number)
 end
 
-p search_array(arr, 4)
+p search_array(arr, 56)
+
+#Release 1: Calculate Fibonacci Numbers
+
+fib_sequence = []
+
+#========
+#Driving code
+p "Please type the Fibonacci sequence element you want to get: "
+element = gets.chomp.to_i
+
+x = element - 2
+
+fib_sequence = x.times.inject([0,1]) do |a, idx|
+    a << a[-2] + a[-1]
+end
+
+p fib_sequence
+
+#Verify the last element of the sequence for 100 elements to be 218922995834555169026
+
+p fib_sequence.max
+
+#Re
+
+
 
 
 
