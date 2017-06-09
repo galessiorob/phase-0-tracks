1b9d759c96d64039aa304aba5f1b1c273582069d
galessio <alessio.gabriela@gmail.com>
2017-06-08 18:08:13 -0700

    Add pseudocode for release 0

---
+++ ruby/problem_solving_methods.rb
@@ -0,0 +1,16 @@
+#Pairing 5.6
+
+#Release 0
+
+#Pseudocode
+#Define an array of random numbers
+#Ask user for an integer
+#Search and check if user input is within the array
+  #If integer matches the array, return the index
+  #Else output "no" or "nil"
+
+#===============================
+#Driving code
+
+
+
