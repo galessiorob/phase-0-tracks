d24f23aadc500c811275ce89c320b2262dbfd351
galessio <alessio.gabriela@gmail.com>
2017-06-10 17:06:23 -0700

    Add methods for each questions

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -4,36 +4,42 @@
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
-# "iNvEsTiGaTiOn".<???>
+
+p "iNvEsTiGaTiOn".swapcase
 # => “InVeStIgAtIoN”
 
-# "zom".<???>
+p "zom".scan(/.{2}|.+/).join("o")
+p "zom".insert(1,"o")
 # => “zoom”
 
-# "enhance".<???>
+
+p "enhance".center(15)
 # => "    enhance    "
 
-# "Stop! You’re under arrest!".<???>
+p "Stop! You’re under arrest!".upcase
 # => "STOP! YOU’RE UNDER ARREST!"
 
-# "the usual".<???>
+p "the usual".concat(" suspects")
 #=> "the usual suspects"
 
-# " suspects".<???>
+p " suspects".insert 0, "the usual "
 # => "the usual suspects"
 
-# "The case of the disappearing last letter".<???>
+p "The case of the disappearing last letter".chop
 # => "The case of the disappearing last lette"
 
-# "The mystery of the missing first letter".<???>
+a = "The mystery of the missing first letter"
+range_end = a.length - 1
+p a.slice!(1..range_end)
 # => "he mystery of the missing first letter"
 
-# "Elementary,    my   dear        Watson!".<???>
+p "Elementary,    my   dear        Watson!".split.join(" ")
 # => "Elementary, my dear Watson!"
 
-# "z".<???>
-# => 122 
-# (What is the significance of the number 122 in relation to the character z?)
+p "z".ord
+# => 122
+# What is the significance of the number 122 in relation to the character z? It's the number from the ASCII standard associated to z, aka the encoding the number for this letter.
 
-# "How many times does the letter 'a' appear in this string?".<???>
+p "How many times does the letter 'a' appear in this string?".count("a")
 # => 4
+
