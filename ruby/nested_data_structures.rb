4532cd45938209ca5572fb47a0d9226c8521d484
galessio <alessio.gabriela@gmail.com>
2017-06-11 13:35:11 -0700

    Build initial nested data structure

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,20 @@
+#Week 5: Design and Build a Nested Data Structure
+
+new_school = {
+  grade_2: {
+    :english => ['grammar', 'spelling', 'reading'],
+    math: ['numbers', 'fractions', 'operations'],
+    science: ['nature', 'motion', 'space']
+  },
+  grade_6: {
+    laguage_arts: {
+      vocabulary: ['synonyms','antonyms', 'homonyms'],
+      comprehension: ['summaries', 'synthesis']
+    },
+    physics: ['speed','mass','energy']
+  }
+}
+
+p new_school
+
+p new_school
