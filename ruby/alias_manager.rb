1894d8b28852e60f9190360b7fb43bed1d572a12
galessio <alessio.gabriela@gmail.com>
2017-06-11 16:29:28 -0700

    Add vowel swapping method

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -12,21 +12,37 @@
 #=============================================
 #Driving code
 
-vowels = {
-  'a' => 'e',
-  'e' => 'i',
-  'i' => 'o',
-  'o' => 'u',
-  'u' => 'a'
-}
+# vowels = {
+#   'a' => 'e',
+#   'e' => 'i',
+#   'i' => 'o',
+#   'o' => 'u',
+#   'u' => 'a'
+# }
 
 consonants = ['bcdfghjklmnpqrstvwxyz']
 
 p "What's your real name?"
 p user_name = (gets.chomp).to_s
-p name1 = user_name.chars
+p name1 = user_name.downcase
 
-def spy_name(user_name)
-  p name1 = user_name.chars
+# def spy_name(name1)
+#   for i in name1
+#     if i.in vowels = true
+# end
+
+# VOWELS = %w(a e i o u)
+SWAP_V = ['a', 'e', 'i', 'o','u']
+SWAP_VR = SWAP_V.rotate 1
+
+def vowel_adv(namestr)
+  namestr.each_char.with_index do |char, i|
+    index = SWAP_V.index char
+    if index
+      namestr[i] = SWAP_VR[index]
+    end
+  end
+  namestr
 end
 
+p vowel_adv(name1)
