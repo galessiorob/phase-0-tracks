e8a95decf35978d95409ff6d62037ac50722fc11
galessio <alessio.gabriela@gmail.com>
2017-06-11 13:55:34 -0700

    Add data calls for different nested structures

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -17,4 +17,16 @@ new_school = {
 
 p new_school
 
-p new_school
+p "Second topic for second grade English"
+puts new_school[:grade_2][:english][1]
+
+p "Last comprehension topic for six grade Language Arts"
+puts new_school[:grade_6][:laguage_arts][:comprehension].last
+
+p "Adding a topic to six grade Physics"
+new_school[:grade_6][:physics].push('optics')
+p new_school[:grade_6][:physics]
+
+p "Changing the value of the first second grade Science topic"
+new_school[:grade_2][:science][0] = 'Biology'
+p new_school[:grade_2][:science][0]
