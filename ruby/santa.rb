103049e8363d028f66337658dd7a7a75a2156e66
galessio <alessio.gabriela@gmail.com>
2017-06-16 13:07:06 -0700

    Add refactored code to bulk add attributes to Santa class

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -26,10 +26,28 @@ end
 #==============================================
 #Driving code
 
-santa = Santa.new('f','latino')
-santa.reindeer_ranking
-
 # Santa.new.speak
-
 # Santa.new.eat_milk_and_cookies("snickerdoodle")
+
+#Add attributes
+# santa = Santa.new('f','latino')
+# santa.reindeer_ranking
+
+#Add more instance examples
+# santas = []
+# santas << Santa.new("agender", "black")
+# santas << Santa.new("female", "Latino")
+# santas << Santa.new("bigender", "white")
+# santas << Santa.new("male", "Japanese")
+# santas << Santa.new("female", "prefer not to say")
+# santas << Santa.new("gender fluid", "Mystical Creature (unicorn)")
+# santas << Santa.new("N/A", "N/A")
+
+#Refactor code to bulk add instances
+santas = []
+example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+example_genders.length.times do |i|
+  santas << Santa.new(example_genders[i], example_ethnicities[i])
+end
 
