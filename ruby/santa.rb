882d5145106d1db6d722510ed2894e4403ab917c
galessio <alessio.gabriela@gmail.com>
2017-06-16 14:32:17 -0700

    Add attribute changing methods

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -2,16 +2,12 @@ class Santa
 
  def initialize(gender,ethnicity)
     p "Initializing Santa instance ..."
-    p @gender = gender
-    p @ethnicity = ethnicity
-    p @age = 0
+    @gender = gender
+    @ethnicity = ethnicity
+    @age = 0
+    @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
   end
 
-  def reindeer_ranking
-    puts @reindeer = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
-    # @reindeer.index.each do
-    #   p @reindeer[index]
-  end
 
   def speak
     p "Ho, ho, ho! Haaaappy holidays!"
@@ -19,6 +15,38 @@ class Santa
 
   def eat_milk_and_cookies(cookie)
     p "That was a good #{cookie}!"
+  end
+
+#Getter methods
+  def age
+    @age
+  end
+
+  def ethnicity
+    @ethnicity
+  end
+
+  def reindeer_ranking
+    @reindeer_ranking
+  end
+
+  def gender
+    @gender
+  end
+
+
+#Setter methods
+  def celebrate_birthday
+    @age = 1
+  end
+
+  def get_mad_at=(bad_reindeer)
+    @reindeer_ranking.delete(bad_reindeer)
+    @reindeer_ranking.push(bad_reindeer)
+  end
+
+  def gender=(new_gender)
+    @gender = new_gender
   end
 
 end
@@ -44,10 +72,23 @@ end
 # santas << Santa.new("N/A", "N/A")
 
 #Refactor code to bulk add instances
-santas = []
-example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
-example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
-example_genders.length.times do |i|
-  santas << Santa.new(example_genders[i], example_ethnicities[i])
-end
+# santas = []
+# example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+# example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+# example_genders.length.times do |i|
+#   santas << Santa.new(example_genders[i], example_ethnicities[i])
+# end
+
+#Attribute changing methods
+santa = Santa.new('f','latino')
+
+santa.celebrate_birthday
+p "New age for santa is #{santa.age}"
+
+santa.get_mad_at = 'Vixen'
+p 'This is the current raking of favorite reindeers'
+puts santa.reindeer_ranking
+
+santa.gender = 'fluid'
+p "New gender for santa is #{santa.gender}"
 
