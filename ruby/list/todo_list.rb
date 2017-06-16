cde2a595853d1e4398da282f21c79edba36d29eb
galessio <alessio.gabriela@gmail.com>
2017-06-16 11:24:47 -0700

    Add characteristics and behavior for puppy class

---
+++ ruby/list/todo_list.rb
@@ -0,0 +1,26 @@
+class TodoList
+
+  # attr_accessor :chores
+
+  def initialize (chores)
+    @chores = chores
+  end
+
+  def get_items (chores)
+    @chores = chores
+  end
+
+  def add_item (item)
+    @chores<<item
+  end
+
+  def delete_item (item)
+    @chores.delete(item)
+    @chores
+  end
+
+  def retrieve_item (index)
+    @chores[index]
+  end
+
+end
---
+++ ruby/puppy.rb
@@ -0,0 +1,21 @@
+#6.1 Introduction to classes
+
+#Release 0
+
+# CLASS ------------------------
+# Puppy
+
+# CHARACTERISTICS ----------------
+# Breed: varies
+# Name: varies
+# Color: varies
+# Friendly: yes
+# Coat: short/medium/long
+# Volume: high/low
+
+# BEHAVIOR -----------------------
+# Bark
+# Jump
+# Fetch
+# Roll Over
+# Play dead
