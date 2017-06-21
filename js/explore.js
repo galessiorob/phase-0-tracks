5aa979a87147ab4027989c8e4f61afb33cc016c1
galessio <alessio.gabriela@gmail.com>
2017-06-20 17:05:52 -0700

    Add pseudocode for reversing function

---
+++ js/explore.js
@@ -0,0 +1,10 @@
+// Week Seven
+// Syntax
+
+// Reversing Function for Strings
+//INPUT: Function can take a string
+//STEPS: Function indexes the string to know the length and position of each character
+//       Reorder characters starting with the last one according to the index
+//       Re-concatenate the characters into a single string again
+//OUTPUT: Function prints the string in reverse on to the console
+
