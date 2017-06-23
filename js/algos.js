46d7bd34c8498e3ff9b2c773b89255cd311123f8
galessio <alessio.gabriela@gmail.com>
2017-06-22 22:41:59 -0700

    Add code to find key-value pairs between two arrays

--- js/algos.js
+++ js/algos.js
@@ -27,14 +27,48 @@ function LongestPhrase(array) {
   console.log(longest);
 }
 
+function MatchFinder (one,two) {
+  this.one = one;
+  this.two = two;
+  var one_s = Object.keys(one).sort();
+  var two_s = Object.keys(two).sort();
+  var one_l = 0;
+  var two_l = 0;
+  var matches = [];
+  var common = false;
+  console.log(one_s);
+  console.log(two_s);
+
+  while (one_l < one_s.length
+        && two_l < two_s.length) {
+          if (one_s[one_l] === two_s[two_l]){
+            matches.push(one_s[one_l]);
+            one_l++;
+            two_l++;
+          }
+          else if(one_s[one_l] < two_s[two_l]){
+            one_l++;
+          }
+          else {
+            b_l++;
+          }
+        }
+ if (matches.length > 0){
+   common = true;
+ }
+// return matches;
+ console.log(common)
+}
+
+
 // =======================================
 
-arr = ["long phrase","mini phrase","longest phrase","an even longer phrase","longer phrase"]
+// arr = ["long phrase","mini phrase","longest phrase","an even longer phrase","longer phrase"]
 
-animals = ["dog","horse","hippopotamus"]
+// animals = ["dog","horse","hippopotamus"]
 
-longphrase = new LongestPhrase(arr)
-// => an even longer phrase
+// longphrase = new LongestPhrase(arr)
+// // => an even longer phrase
 
-longphrasetoo = new LongestPhrase(animals
-  // =>hippopotamus
+// longphrasetoo = new LongestPhrase(animals
+// // =>hippopotamus
