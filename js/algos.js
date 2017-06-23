6f17435852dab5a9c152be47a7b13ad3f9283f45
galessio <alessio.gabriela@gmail.com>
2017-06-22 21:11:26 -0700

    Add code to find longest string in an array

--- js/algos.js
+++ js/algos.js
@@ -6,3 +6,22 @@
 // Repeat this process until all strings have been evaluated and the longest string is stored (defined) as such
 // Output: Longest string according to length
  // ========================================
+
+function LongestPhrase(array) {
+  this.array = array;
+  this.longest;
+  lgth = 0;
+  for(i = 0; i < array.length; i++) {
+    if(array[i].length > lgth){
+      lgth = array[i].length;
+      longest = array[i];
+    }
+  }
+  console.log(longest);
+}
+
+// =======================================
+
+arr = ["long phrase","mini phrase","longest phrase","an even longer phrase","longer phrase"]
+
+longphrase = new LongestPhrase(arr)
