9bedcf4bc2fcd7e6024c150ad84eb4dfdab344fa
galessio <alessio.gabriela@gmail.com>
2017-06-22 21:08:11 -0700

    Add pseudocode to find longest string in an array

---
+++ js/algos.js
@@ -0,0 +1,8 @@
+// Solo Challenge
+
+// Release 0: Find the longest Phrase
+// Input: User provides an array of strings with different lengths
+// Steps: Measure each string's length one by one, if the second string's length is larger than the first one's, define that as the longest.
+// Repeat this process until all strings have been evaluated and the longest string is stored (defined) as such
+// Output: Longest string according to length
+ // ========================================
