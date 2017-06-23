af77fa9c54c73cc19c76079deaccc58f43929850
galessio <alessio.gabriela@gmail.com>
2017-06-22 21:27:06 -0700

    Add pseudocode to check if two objects have a key-value pair in common

--- js/algos.js
+++ js/algos.js
@@ -1,10 +1,17 @@
 // Solo Challenge
 
 // Release 0: Find the longest Phrase
+
 // Input: User provides an array of strings with different lengths
 // Steps: Measure each string's length one by one, if the second string's length is larger than the first one's, define that as the longest.
 // Repeat this process until all strings have been evaluated and the longest string is stored (defined) as such
 // Output: Longest string according to length
+
+// Release 1: Find a Key-Value Match
+
+// Input: Two objects with several key-value pairs
+// Steps: Check if the objects have at least one key-value pair in common, if so return true, if not false
+// Output: True, if there's a match between the two objects, false if not
  // ========================================
 
 function LongestPhrase(array) {
@@ -24,4 +31,10 @@ function LongestPhrase(array) {
 
 arr = ["long phrase","mini phrase","longest phrase","an even longer phrase","longer phrase"]
 
+animals = ["dog","horse","hippopotamus"]
+
 longphrase = new LongestPhrase(arr)
+// => an even longer phrase
+
+longphrasetoo = new LongestPhrase(animals
+  // =>hippopotamus
