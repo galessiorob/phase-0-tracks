8c84ea6b4cc25feb35bc65cee8cff3c97debb7b4
galessio <alessio.gabriela@gmail.com>
2017-07-02 16:29:37 -0700

    Add pseudo code for activity log program

---
+++ databases/activity_log/activity_log.rb
@@ -0,0 +1,6 @@
+#Activity log program
+
+#Pseudocode
+#Input: user types in name, activity type, miles and time
+#Steps: program takes in the users information and stores it as an individual row in a previously created database that collects activity logs from users
+#Output: user's activity gets logged and ranked by activity type and time
