ff5b888676d818d7c9b75e4b087071a0dc6d4cf0
galessio <alessio.gabriela@gmail.com>
2017-07-16 01:00:05 -0700

    Add Beyonce serving pizza

--- web_dev/client_side_js/script.js
+++ web_dev/client_side_js/script.js
@@ -2,6 +2,8 @@ console.log("ojai there!");
 
 var header = document.getElementById("header");
 var content = document.getElementById("content");
+var images = document.getElementById("side-a");
+var bowl = document.getElementById("one");
 
 function over(event) {
   event.target.innerHTML = "Let's get Pizza!";
@@ -15,5 +17,10 @@ function clicky(event) {
   event.target.style.color = "white";
 };
 
+function giffy(event) {
+    event.target.src= 'https://media.giphy.com/media/pVK9JXQ4w1ck/giphy.gif';
+};
+
 header.addEventListener("mouseover", over);
 content.addEventListener("click", clicky);
+bowl.addEventListener("click", giffy);
