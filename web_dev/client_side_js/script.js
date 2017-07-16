c0a8084ad950539f3bb80913ae3d2adf42242748
galessio <alessio.gabriela@gmail.com>
2017-07-16 00:24:31 -0700

    Add last code to js file

--- web_dev/client_side_js/script.js
+++ web_dev/client_side_js/script.js
@@ -9,20 +9,11 @@ function over(event) {
 event.target.style.color = "white";
 };
 
-// function out(event) {
-//   event.target.innerHTML = "Mean Cats rule the world and take over the header!";
-//   event.target.style.backgroundColor = "indianred";
-//   event.target.style.color = "white";
-// };
-
 function clicky(event) {
     event.target.innerHTML = "Dale a tu cuerpo alegria Macarena, Que tu cuerpo es pa darle alegria y cosa buena, Dale a tu cuerpo alegria, Macarena, Hey Macarena!";
   event.target.style.backgroundColor = "indianred";
   event.target.style.color = "white";
 };
 
-
-
 header.addEventListener("mouseover", over);
-// content.addEventListener("mouseout", out);
 content.addEventListener("click", clicky);
